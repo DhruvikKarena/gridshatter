ffe4a248cece41518f9add83bae3e7840f5552ad
Dhruvik Karena <dhruvikkarena1803@gmail.com>
2026-07-18 13:58:18 +0530

    config: configure deploy pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/gridshatter/', // <-- ADD THIS LINE (use your repo name)
   server: {
     port: 3000,
     open: true
